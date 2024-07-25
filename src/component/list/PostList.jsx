cf026c6345e9252aaad4291d6e7afa2b197cb87d
jelee <waftyann@gmail.com>
2024-07-25 17:33:27 +0900

    first commit

---
+++ src/component/list/PostList.jsx
@@ -0,0 +1,20 @@
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`
+
+function PostList(props) {
+  
+  return (
+    <Wrapper>
+
+    </Wrapper>    
+  );
+}
